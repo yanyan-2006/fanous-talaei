9e0c293940d39e5434e23188092d3d26573e221c
yanyan-2006 <maryamadgi16@gmail.com>
2026-08-14 14:02:47 +0330

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,41 @@
 const products = [
   {
     id: 1,
-    name: "محصول نمونه",
-    price: 0,
-    stock: 0,
-    image: "",
-    description: ""
+    name: "لامپ LED حبابی",
+    category: "لامپ",
+    price: "۲۵۰,۰۰۰",
+    stock: 10,
+    image: "💡",
+    description: "لامپ LED کم‌مصرف با نوردهی مناسب."
+  },
+
+  {
+    id: 2,
+    name: "لامپ LED فیلامنتی",
+    category: "لامپ",
+    price: "۳۸۰,۰۰۰",
+    stock: 10,
+    image: "💡",
+    description: "طراحی کلاسیک با نور گرم و ظاهر زیبا."
+  },
+
+  {
+    id: 3,
+    name: "چراغ دیواری مدرن",
+    category: "چراغ",
+    price: "۱,۲۵۰,۰۰۰",
+    stock: 10,
+    image: "✦",
+    description: "مناسب اتاق، راهرو و فضاهای مدرن."
+  },
+
+  {
+    id: 4,
+    name: "چراغ رومیزی طلایی",
+    category: "چراغ",
+    price: "۱,۸۵۰,۰۰۰",
+    stock: 10,
+    image: "◉",
+    description: "طراحی لوکس برای میز کار و دکوراسیون."
   }
 ];
